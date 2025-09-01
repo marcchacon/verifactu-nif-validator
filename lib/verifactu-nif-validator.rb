0548d1cf03356ea995c6b07dc6dd8b4a3bc1337e
Marc Chacon <marcchaconb@gmail.com>
2025-09-01 12:02:22 +0200

    debugging

--- lib/verifactu-nif-validator.rb
+++ lib/verifactu-nif-validator.rb
@@ -2,7 +2,7 @@ require 'savon'
 require 'nokogiri'
 require 'erb'
 
-require_relative 'verifactu-nif-validator/verifactu-nif-xml-builder'
+require_relative 'verifactu-nif-validator/verifactu_nif_xml_builder'
 require_relative 'verifactu-nif-validator/verifactu_nif_validator_error'
 require_relative 'verifactu-nif-validator/verifactu_nif_builder'
 require_relative 'verifactu-nif-validator/contribuyente'
--- lib/verifactu-nif-validator/verifactu_nif_builder.rb
+++ lib/verifactu-nif-validator/verifactu_nif_builder.rb
@@ -2,7 +2,7 @@ module VerifactuNifValidator
   class VerifactuNifBuilder
 
     # Add nif to the list to validate
-    def self.add_nif(nif, name: nil)
+    def self.add_nif(nif:, name: nil)
       @@contribuyentes ||= []
       raise VerifactuNifValidator::VerifactuNifValidatorError, 'A max of 10.000 nifs can be validated at once' if @@contribuyentes.size >= 10_000
       @@contribuyentes << Contribuyente.new(nif, name)
